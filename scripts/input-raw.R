a8241df7eb8f5aa7326f6bda763b05ba32773682
Felipe Figueiredo <philsf79@gmail.com>
2022-01-29 01:28:00 -0300

    filter original huge dataset

---
+++ scripts/input-raw.R
@@ -0,0 +1,20 @@
+# setup -------------------------------------------------------------------
+library(philsfmisc)
+library(data.table)
+library(tidyverse)
+
+# data loading ------------------------------------------------------------
+set.seed(42)
+data.raw <- fread("dataset/FEVS_2020_PRDF.csv") %>%
+  janitor::clean_names()
+
+# data cleaning -----------------------------------------------------------
+
+data.raw <- data.raw %>%
+  select(random_id, agency, q1, q21, q29, q58, dsex) %>%
+  mutate() %>%
+  filter(agency == "NN")
+
+# save dataset ------------------------------------------------------------
+
+write_csv(data.raw, "dataset/FEVS_2020_PRDF_NASA.csv")
