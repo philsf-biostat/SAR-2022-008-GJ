78ca9edf05b6670352851f1d741b85ae87afffd9
Felipe Figueiredo <philsf79@gmail.com>
2022-02-04 18:58:00 -0300

    8- include MH-pooled OR

--- scripts/modeling.R
+++ scripts/modeling.R
@@ -30,20 +30,21 @@ svy_f <- svydesign(ids = ~1,
                    data = analytical %>% filter(dsex == "Female"),
                    weights = ~postwt)
 
-dv_m <- svychisq(~iv + dv, svy_m, statistic = "Chisq") %>% tidy
+dv_m <- svychisq(~iv2 + dv2, svy_m, statistic = "Chisq") %>% tidy
 dv_m_p <- dv_m %>% pull(p.value)
 
-dv_f <- svychisq(~iv + dv, svy_f, statistic = "Chisq") %>% tidy
+dv_f <- svychisq(~iv2 + dv2, svy_f, statistic = "Chisq") %>% tidy
 dv_f_p <- dv_f %>% pull(p.value)
 
-cmh <- svytable(~iv + dv + dsex, svy) %>% mantelhaen.test() %>% tidy
+cmh <- svytable(~iv2 + dv2 + dsex, svy) %>% mantelhaen.test() %>% tidy
 cmh_p <- cmh %>% pull(p.value)
 
 # OR ----------------------------------------------------------------------
 
-or_o <- svytable(~ iv2 + dv2, svy) %>% oddsratio()
+or_o <- svytable(~ iv2 + dv2, svy) %>% oddsratio() %>% suppressWarnings()
 or_o <- or_o$measure[2, ]
-or_m <- svytable(~ iv2 + dv2, svy_m) %>% oddsratio()
+or_m <- svytable(~ iv2 + dv2, svy_m) %>% oddsratio() %>% suppressWarnings()
 or_m <- or_m$measure[2, ]
-or_f <- svytable(~ iv2 + dv2, svy_f) %>% oddsratio()
+or_f <- svytable(~ iv2 + dv2, svy_f) %>% oddsratio() %>% suppressWarnings()
 or_f <- or_f$measure[2, ]
+cmh_or <- cmh[c("estimate", "conf.low", "conf.high")]
